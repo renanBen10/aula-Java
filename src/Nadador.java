e0cbf074eca5db50ca508070664fc1911132aa47
renanBen10 <renanstilo.17@gmail.com>
2020-02-07 22:32:41 -0300

    Aula de Java 1

---
+++ src/Nadador.java
@@ -0,0 +1,30 @@
+
+public class Nadador {
+	public static void main(String[] args) {
+		
+		
+		int idade = 32;
+		
+		
+		if (idade>=5 && idade <=7) {
+			System.out.println("Infantil");
+		}
+			
+			else if (idade>=8 && idade <=10) {
+				System.out.println("Juvenil");
+			}
+		else if (idade>=11 && idade <=15) {
+			System.out.println("Adolescente");
+		}
+		else if (idade>=16 && idade <=30) {
+			System.out.println("Adulto");
+			
+		}
+		else { System.out.println("Sênior");
+			
+		}
+			
+		
+	}
+
+}
---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+
+public class PrimeiroPrograma {
+
+public static void main(String[] args) {
+	
+String nome = "Renan";
+String sobrenome = "Bezerra de Souza";
+int idade = 18;
+
+System.out.println("Olá,"+ nome+" "+ sobrenome);
+System.out.println(String.format("Olá, %s %s %s",nome,sobrenome,idade));
+
+
+}
+}
